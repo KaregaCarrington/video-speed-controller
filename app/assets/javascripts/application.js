1e64d0351474b6d1cd92040f563918fc89dcd493
Rigz777 <rigzgoodmusic@gmail.com>
2017-06-22 22:30:44 -0400

    First commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,25 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+$(document).ready(function(){
+  $("#slider").slider({ min:0, max:2, value:1, step:.01, 
+    change:function(event, ui){
+      $("#myVideo")[0].playbackRate = ui.value
+    } 
+  });
+});
